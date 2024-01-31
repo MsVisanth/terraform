8a2d2f22ebfe4c8e386928521ffaaad5278d9dba
MsVisanth <msaivisanth@gmai.com>
2024-01-31 15:00:17 +0530

    ec2 web server

--- ec2.tf
+++ ec2.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "example" {
     ami             = "ami-08e2c1a8d17c2fe17"
     instance_type   = "t2.micro"
     key_name        = "msv.pem"
-    subnet_id       = ibm-web-sn
+    subnet_id       = aws_subnet.ibm-web-sn.id
     vpc_security_group_ids = aws_security_group.ibm-web-sg.id
-    user_data = file {"ecomm.sh"}
+    user_data = file{"ecomm.sh"}
 
     tags = {
     Name = "ibm-web-server"
