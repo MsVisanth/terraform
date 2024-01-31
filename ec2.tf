72b02d01ddc5f7dd6ef8cd1d4f01cd193adbf30d
MsVisanth <msaivisanth@gmai.com>
2024-01-31 14:45:32 +0530

    create EC2 server

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+# Web Server
+resource "aws_instance" "example" {
+    ami             = "ami-08e2c1a8d17c2fe17"
+    instance_type   = "t2.micro"
+    key_name        = "msv.pem"
+    subnet_id       = ibm-web-sn
+    vpc_security_group_ids = aws_security_group.ibm-web-sg.id
+    user_data = file{"ecomm.sh"}
+
+    tags = {
+    Name = "ibm-web-server"
+  }
+}
